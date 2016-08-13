d0c79061903696d716bca9e7348de4570eeef576
Nathan Pickard <nathan.pickard87@gmail.com>
2016-08-13 13:05:47 -0700

    Added initial chart to page and cleaned up app.js code

--- js/app.js
+++ js/app.js
@@ -141,3 +141,30 @@ function showVotingTable(event) {
       table.appendChild(newImageRow);
     }
 };
+
+var chart;
+
+function drawChart() {
+  var chartProperties = {
+		title:{
+			text: "Voting Results"
+		},
+		data: [
+		{
+			// Change type to "doughnut", "line", "splineArea", etc.
+			type: "column",
+			dataPoints: [
+				{ label: "apple",  y: 10  },
+				{ label: "orange", y: 15  },
+				{ label: "banana", y: 25  },
+				{ label: "mango",  y: 30  },
+				{ label: "grape",  y: 28  }
+			]
+		}
+		]
+	};
+  chart = new CanvasJS.Chart("chart-container", chartProperties);
+	chart.render();
+}
+
+window.addEventListener("load", drawChart);
